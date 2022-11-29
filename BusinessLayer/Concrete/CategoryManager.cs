9fcfe5e92ea0da8d0f1d431a726d5d889eb4f57c
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-29 16:45:41 +0300

    EntityFramework Added.

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,8 @@ namespace BusinessLayer.Concrete
 {
     public class CategoryManager : ICategoryService
     {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
         public void CategorUpdate(Category category)
         {
             throw new NotImplementedException();
