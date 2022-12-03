872c9e9ceed64a59ac45877dffa16aabfa632a3f
EsraNurPalut <nuresrapalut@outlook.com>
2022-12-03 18:31:37 +0300

    SweetAlert Added.

---
+++ Core.Demo/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Demo.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EFWriterRepository());
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Writer p )
+        {
+            p.WriterAbout = "Deneme";
+            p.WriterStatus = true;
+            wm.WriterAdd(p);
+            return RedirectToAction("Index","Blog");
+        }
+    }
+}
