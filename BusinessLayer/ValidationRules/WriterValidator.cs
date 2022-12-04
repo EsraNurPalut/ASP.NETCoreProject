7f869c8413425eea060fa35f41a456d7d3537a21
EsraNurPalut <nuresrapalut@outlook.com>
2022-12-04 15:37:09 +0300

    FluentValidator Added.

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+   public class WriterValidator:AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı soyadı kısmı boş geçilemez");
+
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Yazar mail adresi kısmı boş geçilemez");
+
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Yazar şifre kısmı boş geçilemez");
+
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Yazar adı soyadı kısmı en 2 karakter olmalıdır.");
+
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Yazar adı soyadı kısmı en fazla 50 karakter olmalıdır.");
+        }
+    }
+}
--- Core.Demo/Controllers/RegisterController.cs
+++ Core.Demo/Controllers/RegisterController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -22,10 +24,24 @@ namespace Core.Demo.Controllers
         [HttpPost]
         public IActionResult Index(Writer p )
         {
-            p.WriterAbout = "Deneme";
-            p.WriterStatus = true;
-            wm.WriterAdd(p);
-            return RedirectToAction("Index","Blog");
+            WriterValidator wv = new WriterValidator();
+            ValidationResult results =wv.Validate(p); //validationu kontrol edecek
+            if (results.IsValid) //gecerli ise
+            {
+                p.WriterAbout = "Deneme";
+                p.WriterStatus = true;
+                wm.WriterAdd(p);
+                return RedirectToAction("Index", "Blog");
+
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);   //hata mesajı dönderecek. propertının ismi ve mesajı verri.
+                }
+            }
+            return View();
         }
     }
 }
