46373812371629e4b1e74bc3701ec8836305be14
EsraNurPalut <nuresrapalut@outlook.com>
2022-12-06 22:08:56 +0300

    edit done

--- Core.Demo/Startup.cs
+++ Core.Demo/Startup.cs
@@ -38,9 +38,13 @@ namespace Core.Demo
             });
 
             services.AddMvc();
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie
-        }
+            services.AddAuthentication(
+            CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(x =>
+            {
+                x.LoginPath = "/Login/Index";
+            }
+        );
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
