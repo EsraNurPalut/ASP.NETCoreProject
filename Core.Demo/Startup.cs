7b09fe271571101b1163f3cd21e72f0c6151b52e
EsraNurPalut <nuresrapalut@outlook.com>
2022-12-09 21:39:08 +0300

    Login added.

--- Core.Demo/Startup.cs
+++ Core.Demo/Startup.cs
@@ -38,16 +38,16 @@ namespace Core.Demo
 
                 config.Filters.Add(new AuthorizeFilter(policy));
             });
-        }
-            //    services.AddMvc();
-            //    services.AddAuthentication(
-            //    CookieAuthenticationDefaults.AuthenticationScheme)
-            //        .AddCookie(x =>
-            //    {
-            //        x.LoginPath = "/Login/Index";
-            //    }
-            //);
+            services.AddMvc();  //nereye basarsak basalým hep oturum ac sayfasýnda kalacak.
+            services.AddAuthentication(
+            CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(x =>
+                {
+                    x.LoginPath = "/Login/Index";
+                }
+        );
 
+        }
             // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 
             public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
