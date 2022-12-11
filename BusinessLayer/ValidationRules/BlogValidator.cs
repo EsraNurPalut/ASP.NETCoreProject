37ed99e98e000a8066f0a415c4b93e85d236bafd
EsraNurPalut <nuresrapalut@outlook.com>
2022-12-11 15:24:40 +0300

    Edit done.

--- BusinessLayer/ValidationRules/BlogValidator.cs
+++ BusinessLayer/ValidationRules/BlogValidator.cs
@@ -14,6 +14,9 @@ namespace BusinessLayer.ValidationRules
         {
             RuleFor(x => x.BlogTitle).NotEmpty().WithMessage("Blog başlığını boş geçemezsiniz.");
             RuleFor(x => x.BlogContent).NotEmpty().WithMessage("Blog içeriğini boş geçemezsiniz.");
+            RuleFor(x => x.BlogImage).NotEmpty().WithMessage("Blog  görselini boş geçemezsiniz.");
+            RuleFor(x => x.BlogTitle).MaximumLength(50).WithMessage("Blog başlığı en fazla 50 karakter olmalıdır.");
+            RuleFor(x => x.BlogTitle).MinimumLength(5).WithMessage("Blog başlığı en az 5 karakter olmalıdır.");
         }
     }
 }
