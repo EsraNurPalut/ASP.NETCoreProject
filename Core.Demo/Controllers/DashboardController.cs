b55d97308d769457d469dbf738609dd1f44d5de3
EsraNurPalut <nuresrapalut@outlook.com>
2022-12-17 17:45:57 +0300

    Edit done.

--- Core.Demo/Controllers/DashboardController.cs
+++ Core.Demo/Controllers/DashboardController.cs
@@ -20,6 +20,7 @@ namespace Core.Demo.Controllers
             Context c = new Context();
             ViewBag.v1 = c.Blogs.Count().ToString();
             ViewBag.v2 = c.Blogs.Where(x => x.CategoryID == 1).Count();
+            ViewBag.v3 = c.Categories.Count();
             return View();
         }
     }
