da6dc8b1d3b2c0574973e5e6b9eccf6725320593
EsraNurPalut <nuresrapalut@outlook.com>
2022-12-17 17:41:18 +0300

    Edit done

--- Core.Demo/Controllers/DashboardController.cs
+++ Core.Demo/Controllers/DashboardController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,9 +12,14 @@ namespace Core.Demo.Controllers
 {
     public class DashboardController : Controller
     {
+        BlogManager bm = new BlogManager(new EFBlogRepository());
+
         [AllowAnonymous]
         public IActionResult Index()
         {
+            Context c = new Context();
+            ViewBag.v1 = c.Blogs.Count().ToString();
+            ViewBag.v2=c.Blogs.Where(x=x>)
             return View();
         }
     }
