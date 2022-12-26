52aff568102573a061d4c0369dfc1f93a33810c3
EsraNurPalut <nuresrapalut@outlook.com>
2022-12-26 21:46:50 +0300

    CategoryValidation added

--- Core.Demo/Areas/Admin/Controllers/CategoryController.cs
+++ Core.Demo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -18,6 +19,35 @@ namespace Core.Demo.Areas.Admin.Controllers
         {    
             var values = cm.GetList().ToPagedList(page,3); //topagedlist :baslangıc parametresi(sayfalamaa işlemşn kacıncı sayfadan baslıyor),her bir sayfada senin kac tane degerin olacak. 
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCatgeory(Category category)
+        {
+            BlogValidator bv = new BlogValidator();
+            ValidationResult results = bv.Validate(p); //validationu kontrol edecek
+            if (results.IsValid) //gecerli ise
+            {
+                p.BlogStatus = true;
+                p.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString()); //anın tarihini al
+                bm.TAdd(p);
+                return RedirectToAction("BlogListWithWriter", "Blog");
+
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);   //hata mesajı dönderecek. propertının ismi ve mesajı verri.
+                }
+            }
+            return View();
         }
     }
 }
