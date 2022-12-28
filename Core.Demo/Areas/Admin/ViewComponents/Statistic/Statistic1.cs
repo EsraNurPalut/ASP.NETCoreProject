bdd811c6936d95371ea9385a7fab5d49405a45c6
EsraNurPalut <nuresrapalut@outlook.com>
2022-12-28 22:39:37 +0300

    Srtatistic1 Added.

---
+++ Core.Demo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Demo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
