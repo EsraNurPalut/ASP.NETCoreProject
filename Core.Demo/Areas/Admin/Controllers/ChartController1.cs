e8e5c4ceec1dd4962ee30ed9a20cc891630879ff
EsraNurPalut <nuresrapalut@outlook.com>
2023-01-01 20:23:43 +0300

    Google Chart Added.

---
+++ Core.Demo/Areas/Admin/Controllers/ChartController1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Demo.Areas.Admin.Controllers
+{
+    public class ChartController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            return View();
+        }
+    }
+}
