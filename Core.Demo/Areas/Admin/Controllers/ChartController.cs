58f17607a58f3efd740f881e33b61e536be45c95
EsraNurPalut <nuresrapalut@outlook.com>
2023-01-01 20:51:44 +0300

    Edit done

--- Core.Demo/Areas/Admin/Controllers/ChartController.cs
+++ Core.Demo/Areas/Admin/Controllers/ChartController.cs
@@ -9,6 +9,7 @@ namespace Core.Demo.Areas.Admin.Controllers
 {
     public class ChartController : Controller
     {
+        [Area("Admin")]                 
         public IActionResult Index()
         {
             return View();
